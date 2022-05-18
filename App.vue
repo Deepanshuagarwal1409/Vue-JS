3a76ecb30a8428c3829b622c48760bfaa8933fcc
Deepanshu Agarwal <78287659+Deepanshuagarwal1409@users.noreply.github.com>
2022-05-18 15:03:44 +0530

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <CrudDemo></CrudDemo>
+</template>
+
+<script>
+import CrudDemo from './components/CrudDemo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CrudDemo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
